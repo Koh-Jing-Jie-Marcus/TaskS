812408b0ee495f8c441eb9ce7a91f1fe08a5c19f
Koh_Jing_Jie_Marcus <88085589+Koh-Jing-Jie-Marcus@users.noreply.github.com>
2023-02-17 17:06:11 +0800

    Consume new methods in main

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -16,12 +16,14 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
-        printData(tasksData);
         System.out.println();
-        System.out.println("Printing deadlines");
-        printDeadlines(tasksData);
-
-        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+//        System.out.println("Printing deadlines");
+//        printDeadlines(tasksData);
+//
+//        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+//        printData(tasksData);
+        printDataUsingStreams(tasksData);
+        System.out.println("Total number of deadlines counted using streams: " + countDeadlines(tasksData));
 
     }
 
